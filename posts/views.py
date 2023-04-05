6a9aff42bea42d66e39931b3ed362f51f95e2b56
Kim mingyu <minq0302@naver.com>
2023-04-05 19:14:21 +0900

    dasas

---
+++ posts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.http import require_http_methods
+from .models import Post
+
+
+# Create your views here.
+
+def hello_world(request):
+    if request.method == "GET":
+        return JsonResponse({
+            'status' : 200,
+            'success' : True,
+            'message' : '메시지 전달 성공!',
+            'data' : "Hello world",
+        })
+    
+@require_http_methods(["GET"])
+def get_post_detail(request, id):
+    Post.objects.get
+    post = get_object_or_404(Post, pk=id)
+    category_json={
+        "id" : post.post_id,
+        "writer" : post.writer,
+        "content" : post.content,
+        "category" : post.category,
+    }
+
+    return JsonResponse({
+        'status' : 200,
+        'message' : '게시글 조회 성공',
+        'data' : category_json
+    })
